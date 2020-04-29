242aeeccc84abb975580f82b4a385a66f1744adb
jwatmore <cornflourblue@hotmail.com>
2020-04-29 18:42:34 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# angular-9-basic-authentication-example
+
+Angular 9 - Basic HTTP Authentication Example
+
+For a demo and further details see https://jasonwatmore.com/post/2020/04/29/angular-9-basic-http-authentication-tutorial-example
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    user: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.user.subscribe(x => this.user = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+    }
+}
